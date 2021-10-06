4972f28f67e88975c3ec0e3a3e10d3de263ece08
Greg Reindel <greg.reindel@gmail.com>
2021-10-06 13:13:59 -0500

    rename

--- server/init/config.sh
+++ server/init/config.sh
@@ -5,9 +5,11 @@ for host in ${RELAY_NODE_IP_1} ${RELAY_NODE_IP_2}; do
 relayIPs=$( echo $relayIPs | jq -r '. += ["'${host}'"]')
 done 
 
+hostname=${NODE_HOSTNAME}
+
 echo "
 - path: ${NODE_HOME}/.config.json
   permissions: \"750\"
   content: |
-        $(echo { \"extraParameters\": \"\", \"blockIP\": \"${blockIP}\", \"relayIPs\": ${relayIPs}, \"hostname\": \"${NODE_HOSTNAME}\" } )
+        $(echo { \"extraParameters\": \"\", \"blockIP\": \"${blockIP}\", \"relayIPs\": ${relayIPs}, \"hostname\": \"$hostname\" } )
 "
